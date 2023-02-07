432635d4cd056d90bf43f711bfad5e5a48afc0a1
sanjanap1703 <121236674+sanjanap1703@users.noreply.github.com>
2023-02-07 02:37:39 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

---
+++ my-folder/problems/reverse_integer/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int ans=0;
+        while(n)
+        {
+if(ans>(INT_MAX /10) || ans<(INT_MIN/10))return 0;
+            ans=(ans*10)+(n%10);
+            n=n/10;;
+        }
+        return ans;
+    }
+};;
