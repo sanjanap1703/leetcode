efa7617c7cd7eed1533243c6d4bb51bbe25e60a2
sanjanap1703 <121236674+sanjanap1703@users.noreply.github.com>
2023-08-03 08:00:50 +0000

    Sync LeetCode submission - List the Products Ordered in a Period (mysql)

---
+++ my-folder/problems/list_the_products_ordered_in_a_period/solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select p.product_name, sum(o.unit) as unit from Products p join Orders o on p.product_id=o.product_id where MONTH(o.order_date)=2 and year(o.order_date)=2020 group by p.product_id having sum(o.unit)>=100 ;
