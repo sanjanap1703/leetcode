77590defc03745cc4ed57071e9d2b86f940d6b8a
sanjanap1703 <121236674+sanjanap1703@users.noreply.github.com>
2023-08-03 12:31:39 +0000

    Sync LeetCode submission - Top Travellers (mysql)

---
+++ my-folder/problems/top_travellers/solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select u.name,ifnull(sum(r.distance),0) as travelled_distance from Users u left join Rides r on u.id=r.user_id group by u.id order by sum(r.distance) desc,u.name ;
