f35cf764c669d1d32ab75c5b3fedfbd5df6867da
sanjanap1703 <121236674+sanjanap1703@users.noreply.github.com>
2023-09-29 13:49:36 +0000

    Sync LeetCode submission - Delete Leaves With a Given Value (cpp)

---
+++ my-folder/problems/delete_leaves_with_a_given_value/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int t) {
+        if(!root)
+        {
+            return NULL;
+        }
+        root->left=removeLeafNodes(root->left,t);
+        root->right=removeLeafNodes(root->right,t);
+        if(!root->left && !root->right && root->val==t)
+        {
+            return NULL;
+        }
+        return root;
+        
+    }
+};
